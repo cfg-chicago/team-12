27a5ba7b175254f58346e68bed828a1eb1df768b
Vikram Seshadri <usarocks@gmail.com>
2017-09-30 06:38:42 -0500

    get and post working

--- Team12_Pitch_In/server.js
+++ Team12_Pitch_In/server.js
@@ -5,9 +5,16 @@ var studentController = require('./studentController');
 var bodyParser = require('body-parser');
 // Define the port to run on
 app.set('port', 3000);
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/static')));
-app.post('/api/user/signup', studentController.createStudent);
+app.post('/signup', studentController.createStudent);
+app.get('/signup', studentController.studentSignup);
+
+app.get('/profile/:id', studentController.studentProfile);
+
+
+app.get('/api/user/getStudent', studentController.getStudent);
 // Listen for requests
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
--- Team12_Pitch_In/studentController.js
+++ Team12_Pitch_In/studentController.js
@@ -4,13 +4,36 @@
 // });
 //
 
-function getUser(req, res){
-  console.log(req);
-  res.send('user ' + req.params.id);
+function getStudent(id, callback){
+  var mysql = require('mysql');
+
+  var con = mysql.createConnection({
+    host: "54.234.125.28",
+    user: "root",
+    password: "pass",
+    database: "pitch_in",
+    port:"3306"
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "Select * FROM students WHERE id =" + id;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      con.end();
+      //res.send(result);
+      callback(result);
+    });
+  });
+}
+
+function studentSignup(req, res){
+  res.render("signup", {});
 }
 
 function createStudent(req, res){
-  console.log(req.body);
+  //console.log(req.body);
     var mysql = require('mysql');
 
     var con = mysql.createConnection({
@@ -24,14 +47,23 @@ function createStudent(req, res){
     con.connect(function(err) {
       if (err) throw err;
       console.log("Connected!");
-      var sql = "INSERT INTO students VALUES ('"+req.body.firstName+"','"+req.body.lastName+"','"+req.body.username+"','"+req.body.psw+"','"+req.body.grade+"', 0)";
+      var sql = "INSERT INTO students (firstName, lastName, username, password, Grade, Tickets) VALUES ('"+req.body.firstName+"','"+req.body.lastName+"','"+req.body.username+"','"+req.body.psw+"','"+req.body.grade+"', 0)";
       con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log("1 record inserted");
+        res.redirect("/profile/" + result.insertId);
+        //console.log(result.insertId);
         con.end();
       });
     });
-  res.send('Success!');
+  //res.send('Success!');
+
+}
+
+
+function studentProfile(req, res) {
+  getStudent(req.params.id, function(result){
+    console.log(result[0]);
+    res.render("studentProfile", result[0])});
 }
 
 // function getMonth (monthNumber) {
@@ -40,7 +72,10 @@ function createStudent(req, res){
 
 // export the module
 module.exports = {
-    createStudent: createStudent
+    createStudent: createStudent,
+    studentSignup: studentSignup,
+    studentProfile: studentProfile,
+    getStudent: getStudent
 };
 
 

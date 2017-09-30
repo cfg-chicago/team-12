d07734b680d4fed78aa706aa52d6669aacd6743b
Vikram Seshadri <usarocks@gmail.com>
2017-09-29 22:39:56 -0500

    Fixing up server and organizing html

--- Team12_Pitch_In/server.js
+++ Team12_Pitch_In/server.js
@@ -1,17 +1,14 @@
-// const express = require('express');
-// const app = express();
-// var path = require('path');
-//
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname + '/static'));
-// })
-//
-// app.listen(3000, function () {
-//   console.log('Example app listening on port 3000!')
-// })
-
-
 var express = require('express');
+var path = require('path');
 var app = express();
-app.use('/', express.static(__dirname + '/static'));
-app.listen(3000, function() { console.log('listening')});
+
+// Define the port to run on
+app.set('port', 3000);
+
+app.use(express.static(path.join(__dirname, '/static')));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
